fe02225eb9d1c8a8f97c81448f38f952f2c0cf6d
kavya2492 <kavyabasu2415@gmail.com>
2019-09-25 00:05:14 +0530

    My first java program on Git
    Signed-off-by: kavya2492 <kavyabasu2415@gmail.com>

---
+++ Java0/src/HelloGitWorld.java
@@ -0,0 +1,46 @@
+public class HelloGitWorld {
+	public static void main(String args[]) {
+		System.out.println("HelloWorld");	
+		
+class Add
+		{
+
+int add()
+{
+	int a=10;
+	int b=10;
+	//int sum;
+	return  a+b;	
+}
+int sub()
+{
+	int a=10;
+	int b=9;
+	//int sum;
+	return  a-b;	
+}
+int mul()
+{
+	int a=10;
+	int b=10;
+	//int sum;
+	return  a*b;	
+}
+int div()
+{
+	int a=10;
+	int b=5;
+	//int sum;
+	return  a/b;	
+}
+}
+Add a =new Add();
+System.out.println("addittion   "    + a.add());
+System.out.println("Substraction  "   +a.sub());
+System.out.println("multiplication  "    +a.mul());
+System.out.println("Division   "    +a.div());
+}
+}
+	
+	
+
